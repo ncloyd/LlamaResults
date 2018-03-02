18d58dca22ab66a0a80f38188f8cd18ad1a45810
Addison Albrecht <addisonalbrecht@dhcp-10-105-87-29.wireless.northwestern.private>
2018-03-01 21:04:50 -0600

    resultPhoto incorporated

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,6 +4,7 @@ import LondonLaidBack from "../Cards/London/LondonLaidBack";
 
 const Wrapper = props =>
 <div className="wrapper">
+	<img className="resultPhoto" src="https://raw.githubusercontent.com/ncloyd/LlamaResults/master/images/laidbacklondon.jpg"/>
 	<h5>Itinerary</h5>
 	<LondonLaidBack/>
 	<button type="button" className="btn btn-primary"><h3>Save</h3></button>
